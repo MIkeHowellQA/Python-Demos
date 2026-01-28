e8164efbf3e738df062900f91a52dd66a7a64e5a
Mike Howell <Mike.Howell@TransferenceLtd.co.uk>
2026-01-28 11:27:44 +0000

    28th Jan changes 1

--- Mod 4/Mod 4 - Sets.py
+++ Mod 4/Mod 4 - Sets.py
@@ -1,13 +1,15 @@
 
-staffID= {3729, 3084, 3902, 3317, 2899}
+ # Creating a set, what happens to duplicates?
+staffID= {3729, 3084, 3902, 3317, 2899, 3084}
 print (staffID)
 
 #staffID.add(3840)
-newStaffID={1200, 1300, 1400, 1500}
-staffID.update(newStaffID)
+#
+# newStaffID={1200, 1300, 1400, 1500}
+#staffID.update(newStaffID)
 
-print (staffID)
+#print (staffID)
 
-result= staffID.intersection({3902, 3317})
+#result= staffID.intersection({3902, 3317})
 #print (result)
 
