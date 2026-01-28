f380b9199a1d9fd778ef67b1ff0ce5fee9499ec5
Mike Howell <Mike.Howell@TransferenceLtd.co.uk>
2026-01-28 11:27:50 +0000

    More changes

--- Mod 4/Mod 4 - Sets.py
+++ Mod 4/Mod 4 - Sets.py
@@ -3,13 +3,26 @@
 staffID= {3729, 3084, 3902, 3317, 2899, 3084}
 print (staffID)
 
-#staffID.add(3840)
-#
-# newStaffID={1200, 1300, 1400, 1500}
-#staffID.update(newStaffID)
+"""
+staffID.add(3840)
 
-#print (staffID)
+newStaffID={1200, 1300, 1400, 1500}
+staffID.update(newStaffID)
 
-#result= staffID.intersection({3902, 3317})
-#print (result)
+print (staffID)
 
+result= staffID.intersection({3902, 3317})
+print (result)
+
+# Fill a set with a range of numbers
+
+numbers = set(range(2, 101))
+
+# remove every multiple of a number
+print (numbers)
+numbers = {num for num in numbers if num % 3 != 0}
+print (numbers)
+
+# Remove the first item from a set
+n = numbers.pop()
+"""
